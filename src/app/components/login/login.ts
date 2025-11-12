bf873cbd4d459d7bb3bbe144ad93c31943b12ed2
Mohamed Diop <mohdiopcode@gmail.com>
2025-11-12 21:34:23 +0000

    Finishes login page

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,8 +8,30 @@ import { Router } from '@angular/router';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login implements OnInit {
+export class Login implements OnInit, OnDestroy {
   _isAuthenticated = false;
+  showPassword = false;
+
+  cards = [
+    {
+      title: 'Bénévolat',
+      text: "Impliquez-vous dans des projets à impact social en offrant votre temps et vos compétences.",
+      image: 'volunteering.png'
+    },
+    {
+      title: 'Don',
+      text: "Participez à l’impact positif en apportant un soutien financier, même modeste.",
+      image: 'donation.png'
+    },
+    {
+      title: 'Investissement',
+      text: "Soutenez durablement les projets porteurs et contribuez à leur développement à long terme.",
+      image: 'investment.png'
+    }
+  ];
+
+  currentIndex = 0;
+  intervalId: any;
 
   constructor(private router: Router) {
     const isAuthenticatedString = localStorage.getItem('isAuthenticated')
@@ -17,8 +39,32 @@ export class Login implements OnInit {
   }
 
   ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.nextSlide()
+    }, 4000);
     if (this._isAuthenticated) {
       this.router.navigateByUrl("/home")
     }
   }
+  
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.cards.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  login() {
+
+  }
+  
 }
